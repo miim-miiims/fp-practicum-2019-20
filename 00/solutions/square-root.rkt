d2850e13a7a2917b38db018bf83dafdde84b264b
Maria Bosch <maristbos@gmail.com>
2019-11-10 20:55:46 +0200

    Solution to square root problem

--- 00/solutions/square-root.rkt
+++ 00/solutions/square-root.rkt
@@ -2,4 +2,14 @@
 
 (provide my-sqrt)
 
-(define (my-sqrt x) void)
+(define (my-sqrt n) 
+  (define (helper x e)
+    (let ((f (- (expt x 2) n))
+          (f* (* x 2)))
+      (if (< (abs (/ f f*)) e)
+          x
+          (helper (- x (/ f f*)) e))))
+  (helper (/ n 2.0) 0.0000001))
+    
+        
+        
