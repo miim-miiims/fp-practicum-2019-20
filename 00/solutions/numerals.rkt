3591c274bfbac1ad3b2622efb0ba963d2c8f740f
Maria Bosch <maristbos@gmail.com>
2019-11-10 23:47:28 +0200

    Solution to numeral problem

--- 00/solutions/numerals.rkt
+++ 00/solutions/numerals.rkt
@@ -8,16 +8,34 @@
 
 (define zero (lambda (f v) v))
 
+(define (add1* n)
+  (+ 1 n))
+
 (define (succ n)
   (lambda (f v)
     (f (n f v))))
 
-(define (from-numeral n) void)
+(define (from-numeral n)
+  (n add1* 0))
 
-(define (to-numeral n) void)
+(define (to-numeral n)
+  (if (= n 0)
+      zero
+      (succ (to-numeral (- n 1)))))
 
-(define (plus n m) void)
+(define plus
+  (lambda (n m)
+      (if (= (from-numeral m) 0)
+          n
+          (plus (succ n) (to-numeral (-(from-numeral m) 1))))))
 
-(define (mult n m) void)
+(define mult
+  (lambda (m n)
+    (to-numeral (* (from-numeral n)
+                   (from-numeral m)))))
 
-(define (pred n) void)
+(define pred
+  (lambda (n)
+    (if (equal? n zero)
+        zero
+        (to-numeral (-(from-numeral n) 1)))))
